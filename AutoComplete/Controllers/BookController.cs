3ea714e8b631ed1ef2fb7ada0d7802c2fe591428
Kuntal572 <78458397+Kuntal572@users.noreply.github.com>
2021-10-13 07:27:40 +0530

    Update

--- AutoComplete/Controllers/BookController.cs
+++ AutoComplete/Controllers/BookController.cs
@@ -21,24 +21,13 @@ namespace AutoComplete.Controllers
             int duration = root[0].tripInfos[0].sI[0].duration;
             double hou, min;
             string hour = "0h", minute = "0m";
-            
-                hou = (duration - duration % 60) / 60;
-                min = (duration - hou * 60);
-                            
-                hour = hou.ToString() + "h";
-                minute = min.ToString() + "m";
 
-            
-            //else if(duration == 60)
-            //{
-            //    hour = "1h";
-            //    minute = "0m";
-            //}
-            //else
-            //{
-            //    hour = "0h";
-            //    minute = duration.ToString() + "m";
-            //}
+            hou = (duration - duration % 60) / 60;
+            min = (duration - hou * 60);
+                            
+            hour = hou.ToString() + "h";
+            minute = min.ToString() + "m";
+
             ViewBag.durationh = hour;
             ViewBag.durationm = minute;
             return View(root);
